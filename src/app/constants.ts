5765ef622d403b9673762542e1aa94bc88c4c686
Vladimir <vladimir@solrise.finance>
2023-11-22 12:28:50 +0100

    add constants

---
+++ src/app/constants.ts
@@ -0,0 +1,11 @@
+export const stepTokenImgUrl =
+  "https://assets.coingecko.com/coins/images/14988/standard/step.png";
+
+export const xStepTokenImgUrl =
+  "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/xStpgUCss9piqeFUk2iLVcvJEGhAdJxJQuwLkXP555G/logo.svg";
+
+export const stepLogoImgUrl =
+  "https://app.step.finance/static/media/step.367d541c33d593a1a7e8d1bb26877ea0.svg";
+
+export const stepChartImgUrl =
+  "https://app.step.finance/static/media/step-disconnected-logo.e20a4f8b7497afdf9784416de8ed9862.svg";
---
+++ src/app/stake/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import Swap from "./Swap";
+
+const Stake = () => {
+  return <Swap />;
+};
+
+export default Stake;
